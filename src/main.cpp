ac8690b75c5e0e1fab70b4c715e97d3546c77b4f
tj9913 <terryj1811@gmail.com>
2020-02-10 17:28:17 -0800

    add executioner.cpp and my own main.cpp test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include "../header/Tokenizer.h"
 #include "../header/Token.h"
-
+#include "executioner.cpp"
 //"ls -a; echo test && mkdir hi || echo hello; git status"
 int main() {
-    std::vector<Token*> t = Tokenizer::makeTokens("ls -a; echo test && mkdir hi || echo hello; git status; echo testing && echo watever"); 
-    std::cout << t.size() << std::endl;
-    for (unsigned i = 0; i < t.size(); i++) {
-        std::cout << t.at(i)->val << ' ';
-    }
+    string a = "ls -a; echo test && mkdir hi || echo hello; git status";
+    executioner(a); 
 }
