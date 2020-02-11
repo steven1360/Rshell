3be477a9f9f791538abaf67d08f19dbfa99f8c2f
steven1360 <truongsteven107@gmail.com>
2020-02-10 17:42:17 -0800

    update

--- header/Executor.h
+++ header/Executor.h
@@ -22,11 +22,11 @@ class Executor {
 
             //probably works only if tokens vector size >= 2
             for (unsigned i = 0; i + 1 < tokens.size(); i++) {
-                if (tokens.at(i)->identity == Identity::EXECTOKEN && tokens.at(i+1)->identity == Identity::ARGTOKEN) {
+                if (tokens.at(i)->id == Identity::EXECTOKEN && tokens.at(i+1)->id == Identity::ARGTOKEN) {
                     executable = tokens.at(i);
                     argument = tokens.at(i+1);
                 }
-                else if (tokens.at(i)->identity == Identity::EXECTOKEN && tokens.at(i+1)->identity == Identity::CONNECTORTOKEN) {
+                else if (tokens.at(i)->id == Identity::EXECTOKEN && tokens.at(i+1)->id == Identity::CONNECTORTOKEN) {
                     executable = tokens.at(i);
                     connector = tokens.at(i+1);
                 }
@@ -34,32 +34,49 @@ class Executor {
                     executable = tokens.at(i);
                 }
 
-                if (execute(executable->val, argument->val) == -1 ) {
+                if (execute(executable, argument) == -1 ) {
                     std::cout << "ERROR executing" << std::endl;
                     return;
                 } 
             }
         }
     private:
-        int execute(const std::string& executable, const std::string& argument) {
-            pid_t pid;
+        int execute(Token* exec, Token* arg) {
+	    std::string executable;
+	    std::string argument;
+	    if (exec) {
+		executable = exec->val;
+	    }
+            if (arg) {
+		argument = arg->val;
+	    }
+
+	    std::cout << executable << " " << argument << std::endl;
+
+            int value; 	
             int* status;
             std::vector<std::string> parsedArgs = parseArguments(argument);
-            const char* exec = executable.c_str();
-            const char* arg[parsedArgs.size()];
+            const char* exec_cstr = executable.c_str();
+            char* arg_cstr[parsedArgs.size()];
 
             for (int i = 0; i < parsedArgs.size(); i++) {
-                arg[i] = parsedArgs.at(i).c_str();
+                arg_cstr[i] = const_cast<char*>( parsedArgs.at(i).c_str() );
             }
 
+	    char* testArr[1];
+	    testArr[0] = "-a";
+
             pid_t process = fork();
-            waitpid(0, status, 0);
+            waitpid(0, status,  0);
             if(process == 0){
-                return execvp(exec, arg);
+               value =  execvp(exec_cstr, testArr);    
             }
             else if (process < 0) {
                 std::cout << "fork() failed" << std::endl;
             }
+	    else {
+		return value;
+	    }
         }
 
         std::vector<std::string> parseArguments(const std::string& s) {
