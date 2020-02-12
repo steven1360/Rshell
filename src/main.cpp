11e8dc9fe8f4ac976459f8a4b7bed111c2b0fa1f
tj9913 <terryj1811@gmail.com>
2020-02-12 00:43:02 -0800

    testing my executioner.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../header/Tokenizer.h"
 #include "../header/Token.h"
 #include "../header/Executor.h"
+#include "executioner.cpp"
 
 //"ls -a; echo test && mkdir hi || echo hello; git status"
 int main() {
@@ -9,6 +10,6 @@ int main() {
     for (int i = 0; i < t.size(); i++) {
 	      std::cout << "~~~TOKEN~~~:" << t.at(i)->val << ", id: " << t.at(i)->id  << std::endl;
     }
-    Executor ex(t);
-    ex.execute();
+    std::string a = "ls -a; echo test && mkdir hi || echo hello; git status";
+	executioner(a);
 }
