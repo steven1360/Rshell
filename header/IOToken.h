fb347ee455f37d9863c562aad4807dcda4fa1e9f
steven1360 <truongsteven107@gmail.com>
2020-03-02 14:28:48 -0800

    >

--- header/IOToken.h
+++ header/IOToken.h
@@ -1,16 +1,41 @@
 #pragma once
 #include "Token.h"
+#include<stdio.h> 
+#include <unistd.h> 
+#include <fcntl.h>
 
 class IOToken : public Token {
 	private:
 		std::string op;
 	public:
 		IOToken(const std::string& s) {
-			op = s;
-
+			op = s; 
 		}
 		virtual int execute() {
+
+			std::string r = right->getString();
+			r.pop_back(); // backslash '\' always gets printed at the end for some reason
 			
+			if (op == ">") {
+
+				int fd = open(const_cast<char*>(r.c_str()), O_CREAT | O_APPEND | O_WRONLY, S_IRWXU | S_IRWXG);
+				if (fd < 0) {
+					perror("open");
+				}
+
+				dup2(fd, 1);
+				left->execute();
+				close(fd);
+			}
+			else if (op == "<") {
+
+			}
+			else if (op == ">>") {
+
+			}
+			else if (op == "|") {
+
+			}
 		}
 
 		virtual ID getIdentity() {
